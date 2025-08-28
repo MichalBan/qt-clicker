3bc86665bbba453bdde9ade02a355a69984d377a
MichalBan <michal.banasik.9@gmail.com>
2025-08-28 10:01:09 +0200

    basic clicker game

--- qtclicker.cpp
+++ qtclicker.cpp
@@ -8,6 +8,7 @@ qtclicker::qtclicker(QWidget* parent)
 
 	ui.frameScore->setVisible(false);
 	ui.frameReturn->setVisible(false);
+	ui.frameDots->setVisible(false);
 	connect(ui.buttonExit, &QPushButton::clicked, this, &qtclicker::clickedExit);
 	connect(ui.buttonGo1, &QPushButton::clicked, this, &qtclicker::clickedGo1);
 	connect(ui.buttonReturn, &QPushButton::clicked, this, &qtclicker::clickedReturn);
@@ -26,10 +27,11 @@ void qtclicker::clickedGo1()
 	ui.frameScore->setVisible(true);
 	ui.progressBarTime->setValue(0);
 	ui.labelScore->setText("0");
+	ui.frameDots->setVisible(true);
 
 	tickCounter = 0;
 	score = 0;
-	timerClicker.setInterval(1000);
+	timerClicker.setInterval(tickInterval);
 	connect(&timerClicker, &QTimer::timeout, this, &qtclicker::timerTimeout);
 	timerClicker.start();
 }
@@ -40,26 +42,47 @@ void qtclicker::clickedReturn()
 	ui.frameButtons->setVisible(true);
 }
 
+void qtclicker::clickedDot()
+{
+	++score;
+	ui.labelScore->setText(QString::number(score));
+	delete sender();
+}
+
 void qtclicker::timerTimeout()
 {
 	++tickCounter;
-	if (tickCounter >= maxTick)
+	if (tickCounter > maxTick)
 	{
 		endClicker();
 		return;
 	}
 
-	if (QRandomGenerator::global()->generate())
-	{
-		++score;
-	}
+	spawnDot();
 	ui.progressBarTime->setValue(tickCounter * 100 / maxTick);
-	ui.labelScore->setText(QString::number(score));
 }
 
 void qtclicker::endClicker()
 {
 	ui.frameScore->setVisible(false);
 	ui.frameReturn->setVisible(true);
+	ui.frameDots->setVisible(true);
+	for (auto& child : ui.frameDots->children())
+	{
+		delete child;
+	}
 	timerClicker.stop();
+	ui.labelLastScore->
+	   setText(QString("Your Score: %1 / %2").arg(QString::number(score), QString::number(maxTick)));
+}
+
+void qtclicker::spawnDot()
+{
+	auto dot = new QPushButton(ui.frameDots);
+	int X = QRandomGenerator::global()->bounded(dotSize, ui.frameDots->width() - dotSize);
+	int Y = QRandomGenerator::global()->bounded(dotSize, ui.frameDots->height() - dotSize);
+	dot->setGeometry(X, Y, dotSize, dotSize);
+	dot->setStyleSheet(QString("border-radius: %1px; background-color: rgb(217, 2, 255)").arg(dotSize / 2));
+	connect(dot, &QPushButton::clicked, this, &qtclicker::clickedDot);
+	dot->show();
 }
--- qtclicker.h
+++ qtclicker.h
@@ -12,17 +12,24 @@ public:
     ~qtclicker();
 
 private:
+	// button events
     void clickedExit();
     void clickedGo1();
     void clickedReturn();
+    void clickedDot();
+    // timer events
     void timerTimeout();
+    // helper methods
     void endClicker();
+    void spawnDot();
 
     int tickCounter;
     int score;
     QTimer timerClicker;
     Ui::qtclickerClass ui;
 
+    int dotSize = 20;
+    int tickInterval = 500;
     static constexpr int maxTick = 10;
 };
 
